8a88e59ffcebf5ae6b3d7f3571dc3863e9f40675
maury <maury.martinez@tutanota.com>
2024-04-18 12:56:05 +0200

    Add JobsRoutingModule to JobsModule

--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {JobsRoutingModule} from "./jobs-routing.module";
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    JobsRoutingModule
   ]
 })
 export class JobsModule { }
