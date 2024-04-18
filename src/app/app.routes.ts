f33decf384732ec101dbb9d794e01d92afeea909
maury <maury.martinez@tutanota.com>
2024-04-18 12:50:36 +0200

    Load JobModule in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./jobs/jobs.module').then(m => m.JobsModule)
+  }
+];
