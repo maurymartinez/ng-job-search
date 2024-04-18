a104398998fa5e7c4cb7b1de0d6776116e99c6a4
maury <maury.martinez@tutanota.com>
2024-04-18 14:26:01 +0200

    Declare components in jobs.module.ts

--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {JobsRoutingModule} from "./jobs-routing.module";
+import {JobsComponent} from "./jobs.component";
+import {JobListItemComponent} from "./job-list-item/job-list-item.component";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    JobsComponent,
+    JobListItemComponent
+  ],
   imports: [
     CommonModule,
     JobsRoutingModule
