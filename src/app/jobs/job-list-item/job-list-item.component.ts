a3a2f51d0bac514b2da7aa57a533f6026d5fb65b
maury <maury.martinez@tutanota.com>
2024-04-18 14:24:45 +0200

    Create job-list-item component

---
+++ src/app/jobs/job-list-item/job-list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'njs-jobs-job-list-item',
+  templateUrl: './job-list-item.component.html',
+  styleUrl: './job-list-item.component.css'
+})
+export class JobListItemComponent {
+
+}
