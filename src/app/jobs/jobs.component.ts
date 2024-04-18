1608dec6645373221d4bcfdbf176cf60d8ca4f35
maury <maury.martinez@tutanota.com>
2024-04-18 15:42:56 +0200

    Call job.service.getAllJobs from jobs.component.ts

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Job} from "./entities/job";
+import {JobService} from "./services/job.service";
 
 @Component({
   selector: 'njs-jobs-job',
   templateUrl: './jobs.component.html',
   styleUrl: './jobs.component.css'
 })
-export class JobsComponent {
+export class JobsComponent implements OnInit {
+
+  jobs: Job[] = [];
+
+  constructor(private jobService: JobService) {
+  }
+
+  ngOnInit(): void {
+    this.jobService.getAllJobs()
+      .subscribe(value => this.jobs = value)
+  }
 
 }
