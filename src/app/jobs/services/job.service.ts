fb3853f29d9eb37111af4416e5cf08913562e90b
maury <maury.martinez@tutanota.com>
2024-04-19 20:27:46 +0200

    Implement favorite service methods

--- src/app/jobs/services/job.service.ts
+++ src/app/jobs/services/job.service.ts
@@ -1,16 +1,40 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Job} from "../entities/job";
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getAllJobs(): Observable<Job[]> {
-    return this.http.get<Job[]>("/jobs");
+    return this.http.get<Job[]>("/jobs")
+      .pipe(
+        map(jobs => jobs.map(job => {
+          if (localStorage.getItem(job.id.toString()))
+            job.favorite = true;
+
+          return job;
+        }))
+      );
+  }
+
+  addFavorite(jobId: number) {
+    localStorage.setItem(jobId.toString(), '1');
+  }
+
+  removeFavorite(jobId: number) {
+    localStorage.removeItem(jobId.toString());
+  }
+
+  getFavorites(): Observable<Job[]> {
+    return this.getAllJobs()
+      .pipe(
+        map(jobs => jobs.filter(job => job.favorite))
+      )
   }
 }
