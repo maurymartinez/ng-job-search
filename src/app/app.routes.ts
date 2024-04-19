ef9e7d973fd4a83b08e6b2b5cfb39769ccbdb5e3
maury <maury.martinez@tutanota.com>
2024-04-19 20:28:29 +0200

    Automatic redirect to jobs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import {Routes} from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'jobs',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
     loadChildren: () => import('./jobs/jobs.module').then(m => m.JobsModule)
   }
 ];
